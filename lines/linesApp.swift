0ac7e811283e67fda229eb67734ca08ecfcadb48
Desmond Fitch <desmond.fitch@icloud.com>
2024-02-10 13:51:02 -0500

    Initial Commit

---
+++ lines/linesApp.swift
@@ -0,0 +1,17 @@
+//
+//  linesApp.swift
+//  lines
+//
+//  Created by Desmond Fitch on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct linesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
